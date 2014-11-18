3625cf74c55f470575e30a90562e38ae57fd70fb
Sergey Slavingit <slavin.dev@gmail.com>
2014-11-18 23:50:06 +0200

    Implemented calculator logic.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'json'
+require './lib/calculator'
 
 set :views, 'views'
 set :public_folder, 'assets'
@@ -10,6 +11,33 @@ get '/' do
 end
 
 post '/calc' do
+	
 	content_type :json
-	{status: 200, message:"Success"}.to_json
+
+	total_amount = params[:total_amount]
+	period_number = params[:period_number]
+	percent = params[:percent]
+	type = params[:calc_type]
+
+	if total_amount.empty? or period_number.empty? or percent.empty?
+		return {status: 400, message: "Please, check input data!", data: nil}.to_json
+	end
+
+	calc = Calculator.new
+	
+	calc.credit { |e|
+		e.set_credit_percent = percent
+		e.set_credit_sum = total_amount
+		e.set_credit_term = period_number
+		e.type = type
+	}
+
+	result = calc.result
+
+	unless result.nil?
+		{status: 200, message: "Success!", data: result}.to_json
+	else
+		{status: 400, message: "Error!", data: nil}.to_json
+	end
+
 end
--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,24 +1,126 @@
+class Float
+	def round_and_truncate
+		(self * 100).round / 100.0
+	end
+end
+
 class Calculator
+	
+	attr_accessor :credit_percent, :credit_sum, :credit_term
 
-	attr_accessor :total_amount, :period_number, :percent
+	def initializer
+		self.credit_percent = 0.0
+		self.credit_sum = 0.0
+		self.credit_term = 0.0
+	end
 
-	def self.calc
+	def type=(type)
+		raise "Type should be 'differentiated' or 'annuity'" unless self.include_type? type
+		@type = type
+	end
+
+	def type
+		@type
+	end
+
+	def include_type?(type)
+		(["differentiated", "annuity"].include? type)
+	end
+
+	def credit
 		raise 'Block should be given' unless block_given?
-		instance = Calculator.new
-		yield instance
-		instance
+		yield self
 	end
 
 	def result
-		p self.total_amount
+		raise 'Please, specify type before!' if self.type.nil?
+
+		if type.eql? "differentiated"
+			self.differentiated
+		else
+			self.annuity
+		end
 	end
 
+	def annuity
+		table_of_result_value = []
+
+		rest_of_credit_sum = self.credit_sum
+		
+		1.upto(self.credit_term.to_i) do |month|
+
+			percent = ((self.credit_percent / 12) / 100)
+
+			accrued_percent = self.credit_percent / 100.0
+
+			accrued_interest = rest_of_credit_sum / self.credit_term * accrued_percent	
+
+			main_debt = self.credit_sum * percent / (1 - 1 / (1 + percent) ** self.credit_term)
+			
+			payment_sum = main_debt - accrued_interest
+
+			rest_of_credit_sum -= payment_sum
+
+			table_of_result_value << { payment: payment_sum.round_and_truncate, 
+												percent: accrued_interest.round_and_truncate,
+												rest: rest_of_credit_sum.round_and_truncate,
+												main_debt: main_debt.round_and_truncate,
+												month: month }
+		end
+
+		table_of_result_value
+	end
+
+	def differentiated
+
+		table_of_result_value = []
+
+		rest_of_credit_sum = self.credit_sum
+
+		1.upto(self.credit_term.to_i) do |month|
+
+			percent = self.credit_percent / 100.0
+
+			main_debt = self.credit_sum / self.credit_term
+
+			accrued_interest = rest_of_credit_sum / self.credit_term * percent
+
+			rest_of_credit_sum += accrued_interest
+
+			payment_sum = main_debt + accrued_interest
+
+			rest_of_credit_sum -= payment_sum
+
+			table_of_result_value << { payment: payment_sum.round_and_truncate, 
+												percent: accrued_interest.round_and_truncate,
+												rest: rest_of_credit_sum.round_and_truncate,
+												main_debt: main_debt.round_and_truncate,
+												month: month }
+		end
+
+		table_of_result_value
+	end
+
+	def method_missing(method_name, *args, &block)
+		if method_name =~ /^set_(.+)$/
+			self.set($1, args[0])
+		else
+			super
+		end
+	end
+
+	def set(attr_name, value)
+		if self.respond_to? attr_name
+			self.send("#{attr_name}", value.to_f)
+		end
+	end
 end
 
-calc = Calculator.calc do |e|
-	e.total_amount = 12
-	e.period_number = 4
-	e.percent = 10	
-end
+# calc = Calculator.new
 
-calc.result
+# calc.credit do |e|
+# 	e.set_credit_percent = 20
+# 	e.set_credit_sum = 1000
+# 	e.set_credit_term = 12
+# 	e.type = "annuity"
+# end
